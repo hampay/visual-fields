78f50071bbcb590afd02168daab77ca0c5fe14a5
hampay <hamish.payne@marketpartner.com>
2024-05-29 18:25:01 +0100

    update home page copy

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -7,9 +7,9 @@ const { Title, Paragraph } = Typography;
 const HomePage: React.FC = () => {
   return (
     <div>
-      <Title>Welcome to the Visual Fields Test</Title>
-      <Paragraph>Please follow the instructions to complete the test.</Paragraph>
-      <Link to="/test"><Button type="primary">Start Test</Button></Link>
+      <Title>Visual Fields Test</Title>
+      <Paragraph>Use this test to test your visual fields from home.</Paragraph>
+      <Link to="/test"><Button type="primary">Take the Test</Button></Link>
     </div>
   );
 };
