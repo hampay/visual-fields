9e27d6a04fd8e683471ab282af67c14a46ec4de0
hampay <hamish.payne@marketpartner.com>
2024-05-29 18:34:15 +0100

    use actual crosshairs instead of box

--- src/grid/GridItem.tsx
+++ src/grid/GridItem.tsx
@@ -1,10 +1,12 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 import { FC, ReactNode } from "react"
+
 type GridItemProps = {
     children: ReactNode
     showCrosshair?: boolean
 }
+
 export const GridItem: FC<GridItemProps> = ({ children, showCrosshair }) => {
     const containerStyle = css`
         width: 100%;
@@ -12,17 +14,42 @@ export const GridItem: FC<GridItemProps> = ({ children, showCrosshair }) => {
         display: flex;
         justify-content: center;
         align-items: center;
+        position: relative;
     `
+
     const crosshairsStyle = css`
         display: inline-block;
+        position: relative;
         padding: 30px;
-        ${showCrosshair && css`
-            border: 1px solid #000;
-        `}
     `
-    return <div css={containerStyle}>
-        <div css={crosshairsStyle}>
-            { children }
+
+    const crosshairLinesStyle = showCrosshair && css`
+        &::before, &::after {
+            content: '';
+            position: absolute;
+            background-color: #aaa;
+        }
+        &::before {
+            width: 1px;
+            height: 100%;
+            top: 0;
+            left: 50%;
+            transform: translateX(-50%);
+        }
+        &::after {
+            width: 100%;
+            height: 1px;
+            left: 0;
+            top: 50%;
+            transform: translateY(-50%);
+        }
+    `
+
+    return (
+        <div css={containerStyle}>
+            <div css={[crosshairsStyle, crosshairLinesStyle]}>
+                {children}
+            </div>
         </div>
-    </div>
+    )
 }
